5869954e9d2334898efd55462a3005037a884e91
TonyKr1 <jacruz@ci-sa.com.mx>
2024-05-15 10:12:59 -0600

    Cambio en el modal

--- templates/components/modal.php
+++ templates/components/modal.php
@@ -3,7 +3,7 @@
         <div class="modal-content" style="border-radius: 10px;">
             <div class="modal-header py-3" style="background-color: #009071;color:#FFF;border-top-left-radius: 10px;border-top-right-radius: 10px;">
                 <h5 class="modal-title" id="labelModal"> Title </h5>
-                <button type="button" class="btn-close" id="btnClose" onclick="return false;" aria-label="Close" style="padding: 0;font-size: 22px;"></button>
+                <button type="button" class="btn-close" id="btnClose" onclick="return false;" aria-label="Close" style="padding: 10px;font-size: 22px;z-index: 9;padding-right: 5px;"></button>
             </div>
             <div class="modal-body g-4 row" style="justify-content:center;" id="body_modal">
                 
