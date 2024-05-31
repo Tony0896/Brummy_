e0d7d465b513ceaadbc1d726777a7ffe1408d9b7
Saul Vazquez <svazquez@ci-sa.com.mx>
2024-05-30 19:25:56 -0600

    Prototipo Adjuntar Foto mascota / estilos - Suceess 2024-05-30 19:25

--- functions/mascotas/mascotas.js
+++ functions/mascotas/mascotas.js
@@ -38,11 +38,9 @@ function obtenerMascotas() {
                                 <td>${data.fechaNacimiento}</td>
                                 <td>${data.sexo}</td>
                                 <td>${data.color ? data.color : tdSinData}</td>
-                                <td><div> <div>${volteaFecha(String(data.fechaUlmitoMovimiento).split(" ")[0], 1)} ${
-                                String(String(data.fechaUlmitoMovimiento).split(" ")[1]).split(":")[0]
-                            }:${String(String(data.fechaUlmitoMovimiento).split(" ")[1]).split(":")[1]}</div> <div>${
-                                data.motivoMovimiento
-                            }</div> </div></td>
+                                <td><div> <div>${volteaFecha(String(data.fechaUlmitoMovimiento).split(" ")[0], 1)} ${String(String(data.fechaUlmitoMovimiento).split(" ")[1]).split(":")[0]
+                                }:${String(String(data.fechaUlmitoMovimiento).split(" ")[1]).split(":")[1]}</div> <div>${data.motivoMovimiento
+                                }</div> </div></td>
                                 <td>
                                     <div style="display: flex; flex-direction: row;">
                                         <div class="buttom-blue buttom button-sinText mx-1" title="Ver Perfil" onclick="verMascota(${data.ID})">
@@ -153,6 +151,16 @@ function crearMascota() {
                                                             <option value="">Selecciona una opción</option>
                                                             ${html3}
                                                         </select>
+                                                    </div>
+                                                    <br>
+                                                    <div class="coolinput">
+                                                        <div class="container_upload"> 
+                                                            <div class="header_upload"> 
+                                                                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> 
+                                                                <path d="M7 10V9C7 6.23858 9.23858 4 12 4C14.7614 4 17 6.23858 17 9V10C19.2091 10 21 11.7909 21 14C21 15.4806 20.1956 16.8084 19 17.5M7 10C4.79086 10 3 11.7909 3 14C3 15.4806 3.8044 16.8084 5 17.5M7 10C7.43285 10 7.84965 10.0688 8.24006 10.1959M12 12V21M12 12L15 15M12 12L9 15" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg> <p>Adjuntar una foto de la mascota!</p>
+                                                            </div> 
+                                                            <input id="file" type="file"> 
+                                                            </div>
                                                     </div>
 
                                                 </div>
